20c01239beae25611aa4aa124ee5b14c9d0b9a63
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-05-15 10:31:24 -0600

    Update ROT13.py

--- PICO codes!/ROT13.py
+++ PICO codes!/ROT13.py
@@ -1,7 +1,21 @@
 # cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_hyLicInt}
 # ROT13 just wraps back around after 13 letters
 
+def ROT13(word):
+    lowerLet = 'abcdefghijklmnopqrstuvwxyz'
+    captialLet =  'ABCDEFGHIJKLMONOPQRSTUVWXYZ'
+    deciphered = ''
+    for i in range(len(word)):
+        if word[i].islower():
+            print(i)
+            print("-")
+            print(i % len(word))
+        
+
+
+
 def main():
-    print("hello")
+    # print("hello")
+    ROT13("cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_hyLicInt}")
 
 main()
