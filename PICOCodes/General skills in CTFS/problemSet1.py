c5584deca6a63e53666b22e294f79c257614a592
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-08-07 12:22:16 -0600

    pico

---
+++ PICOCodes/General skills in CTFS/problemSet1.py
@@ -0,0 +1,24 @@
+# Problem set 1
+
+# 1) 2Warm
+# 2) Warmed Up
+# 3) Lets warm Up
+
+
+# 1) 2Warm
+# Can you convert the number 42 (base 10) to binary (base 2)?
+
+# picoCTF{101010}
+
+
+# 2) What is 0x3D (base 16) in decimal (base 10)?
+
+# picoCTF{61}
+
+# 3) If I told you a word started with 0x70 in hexadecimal, what would it start with in ASCII?
+
+# hexadecimal is base 16
+
+# Using from base 16 it converts to 112, in ASCII that is the letter p
+
+# picoCTF{p}
