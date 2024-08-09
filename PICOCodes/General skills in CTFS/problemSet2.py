0835056f7681f7b2ba5b095a2a62c5dbd05ac172
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-08-09 09:17:10 -0600

    Pico CTF

---
+++ PICOCodes/General skills in CTFS/problemSet2.py
@@ -0,0 +1,15 @@
+# picoGym problem set 2
+# -Obedient cat (follow the hints and solve this challenge using the shell)
+# -wave a flag
+# -convertme.py
+# -whats a net cat
+
+# 1) Obedient cat
+# To get the file accessible in your shell, enter the following in the Terminal prompt: 
+# $ wget https://mercury.picoctf.net/static/0e428b2db9788d31189329bed089ce98/flag
+# then use cat flag to see the flag 
+
+# 2) wave a flag
+# get it using wget https://mercury.picoctf.net/static/f95b1ee9f29d631d99073e34703a2826/warm
+# then you should run ./warm in the terminal but however it may not be executable right away
+# so then use chmod +x warm to make it executable then run the thing above
