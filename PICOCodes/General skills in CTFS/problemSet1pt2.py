0421cb567d3f10db086f93ff35b60427699339b7
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-08-11 13:15:55 -0600

    p

---
+++ PICOCodes/General skills in CTFS/problemSet1pt2.py
@@ -0,0 +1,53 @@
+# problem set
+# - Nice netcat
+# -tab, tab attack
+# -python wrangling
+# -magikarp ground mission
+
+# 1) Nice netcat
+numList = [
+112 ,
+105 ,
+99 ,
+111 ,
+67 ,
+84 ,
+70 ,
+123 ,
+103 ,
+48 ,
+48 ,
+100 ,
+95 ,
+107 ,
+49 ,
+116 ,
+116 ,
+121 ,
+33 ,
+95 ,
+110 ,
+49 ,
+99 ,
+51 ,
+95 ,
+107 ,
+49 ,
+116 ,
+116 ,
+121 ,
+33 ,
+95 ,
+97 ,
+102 ,
+100 ,
+53 ,
+102 ,
+100 ,
+97 ,
+52 ,
+125 ,
+10 ,
+]
+for i in numList:
+    pass
