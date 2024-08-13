4adc534c90955c60acb8e333ef94153824094c1a
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-08-13 12:04:45 -0600

    update

--- PICOCodes/General skills in CTFS/problemSet2pt2.py
+++ PICOCodes/General skills in CTFS/problemSet2pt2.py
@@ -9,3 +9,9 @@
 # Can you find the flag in file? This would be really tedious to look through manually, something tells me there is a better way.
 # used grep
 # grep -i "pico" file
+
+# 2) first find
+# Unzip this archive and find the file named 'uber-secret.txt'
+# Download zip file
+
+# next run find -name 'uber-secret.txt' then copy path and nano the text file which will get you your code
