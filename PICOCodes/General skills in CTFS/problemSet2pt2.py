fbed3e055471f984b419bc475203b380e44c9662
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-08-13 11:54:24 -0600

    uploaded file to be used in problemset2pt2.py

---
+++ PICOCodes/General skills in CTFS/problemSet2pt2.py
@@ -0,0 +1,11 @@
+# picoGym problem set 2
+
+# -first grep
+# -first find
+# -big zip
+
+
+# 1) first grep
+# Can you find the flag in file? This would be really tedious to look through manually, something tells me there is a better way.
+# used grep
+# grep -i "pico" file
