e6eebfc4ec8dfd2a04a59d0e45501bd09bd7fa80
RaezanVillamor <62949579+RaezanVillamor@users.noreply.github.com>
2024-09-03 15:52:10 -0600

    update / working on caesar decode

---
+++ PICOCodes/Cryptography and picoGym Learning Intro/new_caesar.py
@@ -0,0 +1,32 @@
+import string
+
+LOWERCASE_OFFSET = ord("a")
+ALPHABET = string.ascii_lowercase[:16]
+# print(ALPHABET) the ALPHABET will be the first 16 used for the key a-p
+def b16_encode(plain):
+	enc = ""
+	for c in plain:
+		binary = "{0:08b}".format(ord(c))
+		enc += ALPHABET[int(binary[:4], 2)]
+		enc += ALPHABET[int(binary[4:], 2)]
+	return enc
+
+def b16_decode(encoded):
+    pass
+
+def shift(c, k):
+	t1 = ord(c) - LOWERCASE_OFFSET
+	t2 = ord(k) - LOWERCASE_OFFSET
+	return ALPHABET[(t1 + t2) % len(ALPHABET)]
+
+flag = "redacted"
+key = "redacted"
+# assert all([k in ALPHABET for k in key])
+# checks for the key in ALPHABET
+# assert len(key) == 1
+
+b16 = b16_encode(flag)
+enc = ""
+for i, c in enumerate(b16):
+	enc += shift(c, key[i % len(key)])
+print(enc)
